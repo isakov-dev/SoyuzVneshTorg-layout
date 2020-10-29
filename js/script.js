08dd8320c57e84b403c147054417510d3926b689
isakov-dev <isakov.developer@gmail.com>
2020-10-29 11:26:39 +0600

    Added tooltips for services

--- js/script.js
+++ js/script.js
@@ -1,17 +1,32 @@
+$(document).ready(function () {
+
+    let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'))
+    let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
+});
+
 $(window).on('load', function () {
 
-    $('.partners__slider').flickity({
-        cellAlign: 'left',
-        contain: true,
-        pageDots: false,
-        percentPosition: false,
-    });
+    let $partners__slider = $('.partners__slider');
+    if ($partners__slider.length) {
+        $partners__slider.flickity({
+            cellAlign: 'left',
+            contain: true,
+            pageDots: false,
+            percentPosition: false,
+        });
+    }
 
-    $('.reviews__slider').flickity({
-        cellAlign: 'left',
-        contain: true,
-        pageDots: false,
-        percentPosition: true,
-    });
+    let $reviews__slider = $('.reviews__slider');
+    if ($reviews__slider.length) {
+        $reviews__slider.flickity({
+            cellAlign: 'left',
+            contain: true,
+            pageDots: false,
+            percentPosition: true,
+        });
+    }
 
 });
